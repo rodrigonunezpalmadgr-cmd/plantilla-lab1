59759311dacb6aab4955b6d62b6e48a9a47094c6
rodri_dgr <rodrigonunezpalma.dgr@gmail.com>
2025-10-07 22:51:47 -0400

    feat: nueva plantilla con angular

---
+++ docs/chunk-L6L4LYWW.js
@@ -0,0 +1,1 @@
+import{Ca as a,Da as n,Fa as i,qa as o}from"./chunk-ENHI5KT7.js";var s=class e{static \u0275fac=function(t){return new(t||e)};static \u0275cmp=o({type:e,selectors:[["app-about"]],decls:2,vars:0,template:function(t,m){t&1&&(a(0,"p"),i(1,"about works!"),n())},encapsulation:2})};export{s as About};
